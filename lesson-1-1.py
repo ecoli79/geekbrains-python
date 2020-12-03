639402d18c0d377913ea559fd58ab1d12d0c6a83
ecoli <impol@yandex.ru>
2020-12-04 00:18:33 +0500

    homework Lesson-1

---
+++ lesson-1-1.py
@@ -0,0 +1,13 @@
+# Lesson-1 example 1
+# working with parameters
+
+name = input("Enter your name: ")
+age = int(input("How old are you?: "))
+
+print(f"Hi, {name}, you already {age} old.")
+
+favorite_season = input("What is your favorite season?: ")
+
+where_from = input("Where you ara from?: ")
+
+print(f"{name}, you are so lucky, you living in the {where_from}. You like so beautiful season - {favorite_season}!")
