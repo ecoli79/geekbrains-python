34b9691188f11e56dba9c93a962347dcff786815
ecoli <impol@yandex.ru>
2020-12-11 21:04:57 +0500

    lesson-3

---
+++ lesson-3/lesson-3-5.py
@@ -0,0 +1,26 @@
+# lesson 3 example 5
+# working with function
+
+def summary_digits(input_string):
+    """Суммирование чисел полученных в параметрах функции"""
+
+    digits = [int(item) for item in input_string.split()]
+    return sum(digits)
+
+
+summ = 0
+
+while True:
+
+    input_data = input(
+        "Введите несколько чисел через запятую для выхода из программы наберите Q: ").upper()
+
+    if "Q" not in input_data:
+        summ += summary_digits(input_data)
+
+        print(f"Сумма введенных чисел равна: {summ}")
+
+    else:
+        summ += summary_digits(input_data.replace("Q", ""))
+        print(f"Сумма введенных чисел равна: {summ}")
+        break
---
+++ lesson-3/lesson-3-6.py
@@ -0,0 +1,17 @@
+# lesson 3 example 6
+# working with function and agruments
+
+def int_func(my_str):
+    """Function for working string"""
+
+    string_out = ""
+    list_words = my_str.split()
+    list_words = [str(el).capitalize() for el in list_words]
+    for el in list_words:
+        string_out += str(el) + " "
+
+    print(string_out)
+
+
+while True:
+    int_func(input("Введите слово или несколько слов разделяя их пробелом: "))
